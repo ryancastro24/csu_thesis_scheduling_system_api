4c7794809c60cf494d0b276cdaeb8b0400825e45
ryanjoel24 <ryanjoelimba@gmail.com>
2025-04-30 16:23:40 +0800

    added change password using otp

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ connectDB();
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "50mb" }));
-app.use(express.raw({ type: "multipart/form-data" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", users);
