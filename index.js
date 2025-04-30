dec6a9c5940b90e3f07ebe2a6c5eeb21bc2e7501
ryanjoel24 <ryanjoelimba@gmail.com>
2025-04-30 15:50:49 +0800

    changes the index.js

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.raw({ type: "multipart/form-data" }));
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", users);
