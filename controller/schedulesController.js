0f980bbb0b7f94be1c86b465333b0bdc9308f68f
ryanjoel24 <ryanjoelimba@gmail.com>
2025-09-28 22:27:24 +0800

    updated the start of the automatic time generator

--- controller/schedulesController.js
+++ controller/schedulesController.js
@@ -47,7 +47,7 @@ export const generateSchedule = async (req, res) => {
 
     // Default working hours (excluding lunch)
     const defaultTimeRanges = [
-      { start: "08:00", end: "12:00" },
+      { start: "09:00", end: "12:00" },
       { start: "13:00", end: "17:00" },
     ];
 
